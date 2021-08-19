d205774c3a2d347c84a1023fac15b715d31ac3b9
BIGDummyHead <47464673+BIGDummyHead@users.noreply.github.com>
2021-08-18 23:21:55 -0400

    added in command for giving items without ID - uses same command, without parse.

--- ModTheGungeonLoader/Bootstrap/DefaultConsole.cs
+++ ModTheGungeonLoader/Bootstrap/DefaultConsole.cs
@@ -94,6 +94,16 @@ namespace Gungeon.Bootstrap
                         if (z != null)
                             $"{z.DisplayName} - added to inventory.".Log(ConsoleColor.Green);
                     }
+                    else{
+                        PickupObject obj = PickupIDs.GetItem(x[0], false) ? PickupIDs.GetItem(x[0], true);
+                        int id = obj.PickupObjectId;
+
+                        obj = PickupIDs.GiveItem(id);
+
+                        if(obj != null)
+                           $"{obj.DisplayName} - added to inventory.".Log(ConsoleColor.Green);
+                           
+                    }
 
                 }, ParseArgument.Create("item"), ParseArgument.Create());
             }
